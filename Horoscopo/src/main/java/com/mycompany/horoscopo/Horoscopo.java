ee20642fdeab4c2c06f09c8f5fd8a43cfa575ec6
rogeriodevweb <piuutrapp@gmail.com>
2026-09-22 17:52:18 -0300

    Criacao das telas com imagem de fundo

---
+++ Horoscopo/src/main/java/com/mycompany/horoscopo/Horoscopo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.horoscopo;
+
+/**
+ *
+ * @author RogerioSilva
+ */
+public class Horoscopo {
+
+    public static void main(String[] args) {
+        
+    }
+}
